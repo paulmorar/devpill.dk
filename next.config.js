744874f75e3881937c627110930052516b278fe4
paulmorar <paulcornelmorar@gmail.com>
2025-01-18 02:10:03 +0100

    Add initial template website

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require('@sentry/nextjs')
+const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    productionBrowserSourceMaps: true,
-    reactStrictMode: true,
-    swcMinify: true,
-}
+const nextConfig = {}
 
-module.exports = withSentryConfig(
-    nextConfig,
-    { silent: true },
-    { hideSourcemaps: true }
-)
+module.exports = withContentlayer(nextConfig)
